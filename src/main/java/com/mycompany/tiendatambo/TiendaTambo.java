8beda3e9b431b05fce6f1a2fa72c772f3439ab90
78022323-lang <78022323@continental.edu.pe>
2026-01-28 21:55:21 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/tiendatambo/TiendaTambo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendatambo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TiendaTambo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
